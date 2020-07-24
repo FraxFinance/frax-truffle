d9504be304fb3a1669c3147e8dedc323aa6469a6
JasonHuan <sino@g.ucla.edu>
2020-07-24 06:56:30 -0700

    testing balanceOf() now works

--- test/tether.js
+++ test/tether.js
@@ -1,14 +1,28 @@
-const tether = artifacts.require("tether");
+var assert = require("assert");
 
-contract("tether", accounts => {
-  it("should need arguments before this runs", () =>
-    tether.deployed()
-      .then(instance => instance.getBalance.call(accounts[0]))
-      .then(balance => {
-        assert.equal(
-          balance.valueOf(),
-          10000,
-          "10000 wasn't in the first account"
-        );
-      }));
+var tether = artifacts.require("tether");
+var frax = artifacts.require("FRAXStablecoin");
+
+
+contract('tether', function(accounts) {
+
+  it("returns the tether balance of account[0]", function() {
+    return tether.deployed().then(function(deployed) {
+      console.log('tether balance of accounts[0]: ');
+      deployed.balanceOf(accounts[0]).then(balanceOfOutput => console.log(balanceOfOutput.words[0]));
+
+      return deployed.balanceOf(accounts[0]).then(balanceOfOutput => balanceOfOutput.words[0]);
+    });
+  });
+
+});
+
+contract('FRAXStablecoin', function(accounts) {
+  it("returns the FRAX balance of account[0]", function() {
+    return frax.deployed().then(function(deployed) {
+      console.log('FRAX balance of accounts[0]: ');
+      return deployed.balanceOf(accounts[0]).then(balanceOfOutput => console.log(balanceOfOutput.words[0]));
+    });
+  });
+
 });
