69d225d15cefd0321d0d6dfe633c8a690e8a73b8
jasonhuan <sino@g.ucla.edu>
2020-09-01 16:31:50 -0700

    new contracts

--- contracts/frax.sol
+++ contracts/frax.sol
@@ -4,282 +4,9 @@ import "./Context.sol";
 import "./IERC20.sol";
 import "./SafeMath.sol";
 import "./fxs.sol";
+import "./frax_pool.sol";
 
 
-/**
- * @dev Implementation of the {IERC20} interface.
- *
- * This implementation is agnostic to the way tokens are created. This means
- * that a supply mechanism has to be added in a derived contract using {_mint}.
- * For a generic mechanism see {ERC20Mintable}.
- *
- * TIP: For a detailed writeup see our guide
- * https://forum.zeppelin.solutions/t/how-to-implement-erc20-supply-mechanisms/226[How
- * to implement supply mechanisms].
- *
- * We have followed general OpenZeppelin guidelines: functions revert instead
- * of returning `false` on failure. This behavior is nonetheless conventional
- * and does not conflict with the expectations of ERC20 applications.
- *
- * Additionally, an {Approval} event is emitted on calls to {transferFrom}.
- * This allows applications to reconstruct the allowance for all accounts just
- * by listening to said events. Other implementations of the EIP may not emit
- * these events, as it isn't required by the specification.
- *
- * Finally, the non-standard {decreaseAllowance} and {increaseAllowance}
- * functions have been added to mitigate the well-known issues around setting
- * allowances. See {IERC20-approve}.
- */
-contract ERC20 is Context, IERC20 {
-    using SafeMath for uint256;
-
-    mapping (address => uint256) private _balances;
-
-    mapping (address => mapping (address => uint256)) private _allowances;
-
-    uint256 private _totalSupply;
-
-    /**
-     * @dev See {IERC20-totalSupply}.
-     */
-    function totalSupply() public view override returns (uint256) {
-        return _totalSupply;
-    }
-
-    /**
-     * @dev See {IERC20-balanceOf}.
-     */
-    function balanceOf(address account) public view override returns (uint256) {
-        return _balances[account];
-    }
-
-    /**
-     * @dev See {IERC20-transfer}.
-     *
-     * Requirements:
-     *
-     * - `recipient` cannot be the zero address.
-     * - the caller must have a balance of at least `amount`.
-     */
-    function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
-        _transfer(_msgSender(), recipient, amount);
-        return true;
-    }
-
-    /**
-     * @dev See {IERC20-allowance}.
-     */
-    function allowance(address owner, address spender) public view virtual override returns (uint256) {
-        return _allowances[owner][spender];
-    }
-
-    /**
-     * @dev See {IERC20-approve}.
-     *
-     * Requirements:
-     *
-     * - `spender` cannot be the zero address.approve(address spender, uint256 amount)
-     */
-    function approve(address spender, uint256 amount) public virtual override returns (bool) {
-        _approve(_msgSender(), spender, amount);
-        return true;
-    }
-
-    /**
-     * @dev See {IERC20-transferFrom}.
-     *
-     * Emits an {Approval} event indicating the updated allowance. This is not
-     * required by the EIP. See the note at the beginning of {ERC20};
-     *
-     * Requirements:
-     * - `sender` and `recipient` cannot be the zero address.
-     * - `sender` must have a balance of at least `amount`.
-     * - the caller must have allowance for `sender`'s tokens of at least
-     * `amount`.
-     */
-    function transferFrom(address sender, address recipient, uint256 amount) public virtual override returns (bool) {
-        _transfer(sender, recipient, amount);
-        _approve(sender, _msgSender(), _allowances[sender][_msgSender()].sub(amount, "ERC20: transfer amount exceeds allowance"));
-        return true;
-    }
-
-    /**
-     * @dev Atomically increases the allowance granted to `spender` by the caller.
-     *
-     * This is an alternative to {approve} that can be used as a mitigation for
-     * problems described in {IERC20-approve}.
-     *
-     * Emits an {Approval} event indicating the updated allowance.
-     *
-     * Requirements:
-     *
-     * - `spender` cannot be the zero address.
-     */
-    function increaseAllowance(address spender, uint256 addedValue) public virtual returns (bool) {
-        _approve(_msgSender(), spender, _allowances[_msgSender()][spender].add(addedValue));
-        return true;
-    }
-
-    /**
-     * @dev Atomically decreases the allowance granted to `spender` by the caller.
-     *
-     * This is an alternative to {approve} that can be used as a mitigation for
-     * problems described in {IERC20-approve}.
-     *
-     * Emits an {Approval} event indicating the updated allowance.
-     *
-     * Requirements:
-     *
-     * - `spender` cannot be the zero address.
-     * - `spender` must have allowance for the caller of at least
-     * `subtractedValue`.
-     */
-    function decreaseAllowance(address spender, uint256 subtractedValue) public virtual returns (bool) {
-        _approve(_msgSender(), spender, _allowances[_msgSender()][spender].sub(subtractedValue, "ERC20: decreased allowance below zero"));
-        return true;
-    }
-
-    /**
-     * @dev Moves tokens `amount` from `sender` to `recipient`.
-     *
-     * This is internal function is equivalent to {transfer}, and can be used to
-     * e.g. implement automatic token fees, slashing mechanisms, etc.
-     *
-     * Emits a {Transfer} event.
-     *
-     * Requirements:
-     *
-     * - `sender` cannot be the zero address.
-     * - `recipient` cannot be the zero address.
-     * - `sender` must have a balance of at least `amount`.
-     */
-    function _transfer(address sender, address recipient, uint256 amount) internal virtual {
-        require(sender != address(0), "ERC20: transfer from the zero address");
-        require(recipient != address(0), "ERC20: transfer to the zero address");
-
-        _beforeTokenTransfer(sender, recipient, amount);
-
-        _balances[sender] = _balances[sender].sub(amount, "ERC20: transfer amount exceeds balance");
-        _balances[recipient] = _balances[recipient].add(amount);
-        emit Transfer(sender, recipient, amount);
-    }
-
-    /** @dev Creates `amount` tokens and assigns them to `account`, increasing
-     * the total supply.
-     *
-     * Emits a {Transfer} event with `from` set to the zero address.
-     *
-     * Requirements
-     *
-     * - `to` cannot be the zero address.
-     */
-    function _mint(address account, uint256 amount) internal virtual {
-        require(account != address(0), "ERC20: mint to the zero address");
-
-        _beforeTokenTransfer(address(0), account, amount);
-
-        _totalSupply = _totalSupply.add(amount);
-        _balances[account] = _balances[account].add(amount);
-        emit Transfer(address(0), account, amount);
-    }
-
-    /**
-     * @dev Destroys `amount` tokens from the caller.
-     *
-     * See {ERC20-_burn}.
-     */
-    function burn(uint256 amount) public virtual {
-        _burn(_msgSender(), amount);
-    }
-
-    /**
-     * @dev Destroys `amount` tokens from `account`, deducting from the caller's
-     * allowance.
-     *
-     * See {ERC20-_burn} and {ERC20-allowance}.
-     *
-     * Requirements:
-     *
-     * - the caller must have allowance for `accounts`'s tokens of at least
-     * `amount`.
-     */
-    function burnFrom(address account, uint256 amount) public virtual {
-        uint256 decreasedAllowance = allowance(account, _msgSender()).sub(amount, "ERC20: burn amount exceeds allowance");
-
-        _approve(account, _msgSender(), decreasedAllowance);
-        _burn(account, amount);
-    }
-
-
-    /**
-     * @dev Destroys `amount` tokens from `account`, reducing the
-     * total supply.
-     *
-     * Emits a {Transfer} event with `to` set to the zero address.
-     *
-     * Requirements
-     *
-     * - `account` cannot be the zero address.
-     * - `account` must have at least `amount` tokens.
-     */
-    function _burn(address account, uint256 amount) internal virtual {
-        require(account != address(0), "ERC20: burn from the zero address");
-
-        _beforeTokenTransfer(account, address(0), amount);
-
-        _balances[account] = _balances[account].sub(amount, "ERC20: burn amount exceeds balance");
-        _totalSupply = _totalSupply.sub(amount);
-        emit Transfer(account, address(0), amount);
-    }
-
-    /**
-     * @dev Sets `amount` as the allowance of `spender` over the `owner`s tokens.
-     *
-     * This is internal function is equivalent to `approve`, and can be used to
-     * e.g. set automatic allowances for certain subsystems, etc.
-     *
-     * Emits an {Approval} event.
-     *
-     * Requirements:
-     *
-     * - `owner` cannot be the zero address.
-     * - `spender` cannot be the zero address.
-     */
-    function _approve(address owner, address spender, uint256 amount) internal virtual {
-        require(owner != address(0), "ERC20: approve from the zero address");
-        require(spender != address(0), "ERC20: approve to the zero address");
-
-        _allowances[owner][spender] = amount;
-        emit Approval(owner, spender, amount);
-    }
-
-    /**
-     * @dev Destroys `amount` tokens from `account`.`amount` is then deducted
-     * from the caller's allowance.
-     *
-     * See {_burn} and {_approve}.
-     */
-    function _burnFrom(address account, uint256 amount) internal virtual {
-        _burn(account, amount);
-        _approve(account, _msgSender(), _allowances[account][_msgSender()].sub(amount, "ERC20: burn amount exceeds allowance"));
-    }
-
-    /**
-     * @dev Hook that is called before any transfer of tokens. This includes
-     * minting and burning.
-     *
-     * Calling conditions:
-     *
-     * - when `from` and `to` are both non-zero, `amount` of `from`'s tokens
-     * will be to transferred to `to`.
-     * - when `from` is zero, `amount` tokens will be minted for `to`.
-     * - when `to` is zero, `amount` of `from`'s tokens will be burned.
-     * - `from` and `to` are never both zero.
-     *
-     * To learn more about hooks, head to xref:ROOT:using-hooks.adoc[Using Hooks].
-     */
-    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual { }
-}
 
 
 contract FRAXStablecoin is ERC20 {
@@ -287,11 +14,7 @@ contract FRAXStablecoin is ERC20 {
     string public symbol;
     uint8 public decimals = 18;
     address[] public owners;
-    uint256 ownerCount; //number of different addresses that hold FRAX
-    mapping(address => uint256) public balances;
 
-    
-    //an array of frax pool addresses for future use
     //the addresses in this array are added by the oracle and these contracts are able to mint frax
     address[] frax_pools_array;
 
@@ -299,31 +22,24 @@ contract FRAXStablecoin is ERC20 {
     mapping(address => bool) public frax_pools; 
     
     mapping(address => uint256) public pool_prices;
-    //add frax hop and backstep contracts here and other future monetary policy contracts 
+    
+    //add other future monetary policy contracts to this
     mapping(address => bool) public frax_monetary_policy_contracts;
 
-    
-
-    uint256 phase2_startTime; //epoch time of phase 2 start, fractional phase 
-    uint256 last_hop_time; //epoch time of last FRAX expansion
     uint256 public FRAX_price; //6 decimals of precision
-    uint256 public FRAX_supply; //6 decimals of precision
     uint256 public FXS_price; //6 decimals of precision
     uint256 public global_collateral_ratio; //6 decimals of precision, e.g. 924102 = 0.924102
-    address oracle_address; //this is the address that can change the FRAX and FXS price
-    ERC20 collateral_token;
-    uint256 public redemption_fee;
+    address oracle_address; 
+    uint256 public redemption_fee; //6 decimals of precision, divide by 1000000 in calculations for fee
+    uint256 public minting_fee; //6 decimals of precision, divide by 1000000 in calculations for fee
 
-    uint256 lastCollectionTime; //last time user collected interest on their frax
-    uint256 interestRatePerDay; //the current interest rate offered for frax paid in FXS
-     
     modifier onlyMonPol() {
-       require(frax_monetary_policy_contracts[msg.sender] = true, "only frax expansion-retraction contracts can use this!");
+       require(frax_monetary_policy_contracts[msg.sender] == true, "only frax monetary policy contracts can use this!");
         _;
     } 
      
     modifier onlyPools() {
-       require(frax_pools[msg.sender] = true, "only frax pools can mint new FRAX");
+       require(frax_pools[msg.sender] == true, "only frax pools can mint new FRAX");
         _;
     } 
     
@@ -343,13 +59,18 @@ contract FRAXStablecoin is ERC20 {
 }
 
     //public implementation of internal _mint()
-    function mint(uint256 amount) public virtual onlyMonPol {
+    function mint(uint256 amount) public virtual onlyByOracle {
         _mint(msg.sender, amount);
     }
 
-    //used by pools when user redeems1t1
+    //used by pools when user redeems
     function poolBurn(uint256 amount) public onlyPools {
         _burn(tx.origin,amount);
+    }
+
+    //this function is what other frax pools will call to mint new FRAX 
+    function pool_mint(address m_address, uint256 m_amount) public onlyPools {
+        super._mint(m_address, m_amount);
     }
 
     //adds collateral addresses supported, such as tether and busd, must be ERC20 
@@ -358,26 +79,63 @@ contract FRAXStablecoin is ERC20 {
         frax_pools_array.push(pool_address);
     }
 
+
+
     //adds the monetary policy contracts, hop, backstep etc 
     function setMonetaryPolicyContract(address con_address) public onlyByOracle {
         frax_monetary_policy_contracts[con_address] = true; 
     }
 
-    event storeFRAXPrice(uint256 current_FRAX_supply);
 
-    // the updated price must be within 10% of the old price
-    // this is to prevent accidental mispricings 
-    // a change of greater than 10% requires multiple transactions
-    //need to create this logic
+    //When an oracle contract is deployed in the future,  
+    // the updated price must be within 10% of the old price as reported by the contract, this is to prevent accidental mispricings, a change of greater than 10% requires multiple transactions
+    //this logic is done by the offchain oracle in v1 for simplicity
+
     function setPrices(uint256 FRAX_p,uint256 FXS_p) public onlyByOracle {
         FRAX_price = FRAX_p;
         FXS_price = FXS_p;
-
-        emit storeFRAXPrice(FRAX_supply);
     }
     
+
+    //iterate through all frax pools and calculate all value of collateral in all pools globally 
+    function globalCollateralValue() public view returns (uint256) {
+        uint256 pool_value;
+        uint256 total_collateral_value; 
+        
+        for (uint i=0; i<frax_pools_array.length; i++){
+            ERC20 pool_token = ERC20(frax_pools_array[i]);
+            pool_value = pool_token.balanceOf(frax_pools_array[i]) * frax_pool(frax_pools_array[i]).collateral_price_int();
+            total_collateral_value = total_collateral_value + pool_value;
+   
+    }
+    return total_collateral_value;
+}
+    
+    //there needs to be a time interval that this can be called. Otherwise it can be called multiple times per expansion.
+    uint256 last_call_time; //last time the setNewCollateralRatio function was called
+    function setNewCollateralRatio() public {
+        require(block.timestamp - last_call_time >= 3600 && FRAX_price != 1000000);  //3600 seconds means can be called once per hour, 86400 seconds is per day, callable only if FRAX price is not $1
+        
+        uint256 tot_collat_value =  globalCollateralValue();
+        uint256 globalC_ratio = totalSupply().div(tot_collat_value); //div by 12 places, FRAX has 18 precision and c_ratio has 6              
+        
+        //step increments are .5% 
+        if (FRAX_price > 1000000) {
+            global_collateral_ratio = globalC_ratio - 5000;
+        }    
+        else {
+            global_collateral_ratio = globalC_ratio + 5000;
+        }
+    last_call_time = block.timestamp; //set the time of the last expansion
+            
+    }
+        
+
+    
+
+    
     function setGlobalCollateralRatio(uint256 coll_ra) public onlyByOracle {
-        require(coll_ra < 1000000, "collateral ratio must have 6 decimals of precision and never go above 0.999999");
+        require(coll_ra < 1000001, "collateral ratio must have 6 decimals of precision and never go above 1.0000000");
         global_collateral_ratio = coll_ra;
     }
 
@@ -389,28 +147,14 @@ contract FRAXStablecoin is ERC20 {
         redemption_fee = red_fee;
     }
 
-    
-    function n_collateral_ratio() public view returns (uint256) {
-        return  collateral_token.balanceOf(address(this)).div(totalSupply());
-        
-    }
+     function setMintingFee(uint256 min_fee) public onlyByOracle {
+        minting_fee = min_fee;
+    }  
     
     
-    
-    //this function is what other frax pools will call to mint new FRAX 
-    function pool_mint(address m_address, uint256 m_amount) public onlyPools {
-        super._mint(m_address, m_amount);
-    }
-    
-    //this function also needs to be called any time there is a transfer of FRAX so that interest is paid
-    function collect_interest() public {
-    uint256 daysSinceLastCollect = now.sub(lastCollectionTime).div(86400); // 86400 sec in 1 day
-    uint256 newInterests = daysSinceLastCollect.mul(interestRatePerDay).mul(balanceOf(msg.sender));
 
-    _mint(msg.sender, newInterests);
+    
 
-    lastCollectionTime = now;
-}
 
     
 }
